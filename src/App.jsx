00eab41c6e3ad80d7c7a321135c583e98546c388
saifChebli <cheblii.saifeddine@gmail.com>
2025-08-19 01:58:59 +0100

    filter using completed prop

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,7 @@ import React , { useState } from 'react'
 
 const App = () => {
 
- const [todos , setTodos] = useState([
-   {id : 1 , text : 'Learn React basics' , completed : false},
-   {id : 2 , text : 'Practice useState hook' , completed : true}
- ])
+ const [todos , setTodos] = useState([])
  
  // todos type : Array
 
@@ -14,6 +11,12 @@ const App = () => {
 
 
  const [newTask , setNewTask] = useState('') // take the value from input and store it
+
+ const [filter , setFilter] = useState('all')
+
+
+
+
 
  
  const addTodo = () => {
@@ -39,46 +42,81 @@ const App = () => {
 
 //  add(8 , 9)
  
+console.log(filter);
+
+const filteredTodos = todos.filter(todo => {
+  if (filter === 'completed') return todo.completed
+  if (filter === 'pending') return !todo.completed
+  return true
+})
+
+console.log(filteredTodos);
+
+
+
 
   return (
-    <div className='mx-auto max-w-md mt-10 bg-white shadow-lg p-6'>
-      <h2 className='text-2xl font-bold text-center mb-4'>TODO List</h2>
+    <div className="mx-auto max-w-md mt-10 bg-white shadow-lg p-6">
+      <h2 className="text-2xl font-bold text-center mb-4">TODO List</h2>
       {/* Input and Add Button */}
-      <div className='flex gap-2 mb-4'>
-         <input 
-          type="text" 
-          placeholder='Enter a task...' 
-          className='flex-1 px-3 py-2 border border-gray-300 rounded' 
+      <div className="flex gap-2 mb-4">
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          className="flex-1 px-3 py-2 border border-gray-300 rounded"
           value={newTask}
-          onChange={e => setNewTask(e.target.value)}
-         />
-         <button onClick={addTodo} className='bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded cursor-pointer'>Add</button>
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button
+          onClick={addTodo}
+          className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded cursor-pointer"
+        >
+          Add
+        </button>
       </div>
 
       {/* Filter Buttons */}
 
-      <div className='flex justify-center gap-2 mb-4'>
-        <button className='bg-blue-500 text-white px-3 py-1 rounded'>All</button>
-        <button className='bg-blue-500 text-white px-3 py-1 rounded'>Completed</button>
-        <button className='bg-blue-500 text-white px-3 py-1 rounded'>Pending</button>
+      <div className="flex justify-center gap-2 mb-4">
+        <button
+          onClick={() => setFilter("all")}
+          className={`${filter === 'all' ? 'bg-gray-200 text-black' : 'bg-blue-500 text-white'} px-3 py-1 cursor-pointer rounded`}
+        >
+          All
+        </button>
+        <button
+          onClick={() => setFilter("completed")}
+          className={`${filter === 'completed' ? 'bg-gray-200 text-black' : 'bg-blue-500 text-white'} px-3 py-1 cursor-pointer rounded`}
+        >
+          Completed
+        </button>
+        <button
+          onClick={() => setFilter("pending")}
+          className={`${filter === 'pending' ? 'bg-gray-200 text-black' : 'bg-blue-500 text-white'} px-3 py-1 cursor-pointer rounded`}
+        >
+          Pending
+        </button>
       </div>
 
       {/* Todo List */}
-      <ul className='space-y-2'>
-          {
-            todos.map(todo => (
-              <li 
-                onClick={() => toggleTodo(todo.id)}
-                className={`cursor-pointer px-3 py-2 shadow rounded 
-                  ${todo.completed ? 'line-through text-gray bg-gray-100' : 'bg-gray-50 hover:bg-gray-100'}`}
-              >
-                 {todo.text}
-              </li>
-            ))
-          }
+      <ul className="space-y-2">
+        {filteredTodos.map((todo) => (
+          <li
+            key={todo.id}
+            onClick={() => toggleTodo(todo.id)}
+            className={`cursor-pointer px-3 py-2 shadow rounded 
+                  ${
+                    todo.completed
+                      ? "line-through text-gray bg-gray-100"
+                      : "bg-gray-50 hover:bg-gray-100"
+                  }`}
+          >
+            {todo.text}
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export default App
